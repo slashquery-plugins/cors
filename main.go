1bbd44d4773ef1316fae3be8251eef0301334833
nbari <nbari@tequila.io>
2017-05-29 23:20:03 +0200

    CORS

---
+++ main.go
@@ -0,0 +1,15 @@
+package cors
+
+import (
+	"log"
+	"net/http"
+)
+
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("CORS init")
+		w.Header().Set("sq-cors-version", "1.0.0")
+		r.Header.Set("sq-cors-version", "1.0.0")
+		next.ServeHTTP(w, r)
+	})
+}
