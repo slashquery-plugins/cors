bb217abd9e704f854acf184910678afe5966eb21
nbari <nbari@tequila.io>
2017-05-31 01:16:48 +0200

    sync

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package cors
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func CORS(next http.Handler) http.Handler {
+	time := time.Now().String()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("CORS init")
-		w.Header().Set("sq-cors-version", "1.0.0")
-		r.Header.Set("sq-cors-version", "1.0.0")
+		w.Header().Set("sq-cors-version", time)
+		r.Header.Set("sq-cors-version", time)
 		next.ServeHTTP(w, r)
 	})
 }
